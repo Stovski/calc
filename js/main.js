b2d75bdefaea44c611e163eadcfbbd21cda09987
teacher <teacher@stepdn.com>
2018-12-20 16:50:04 +0300

    4

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    $("div.wrapp-button").click(()=>{
+      $("div.menu").toggleClass("hidden-menu");
+    });
+    $("div.menu__button").click(()=>{
+        $("div.menu").toggleClass("hidden-menu");
+    })
+  });
